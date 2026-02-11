20531f0d7ae1663d0a0d548c1067a7ebfbf19d4e
Muhammad Reza Pahlawan <muhammad_reza.si@upnjatim.ac.id>
2026-02-11 10:17:38 +0700

    Update turnamen.java

--- turnamenbola/turnamen.java
+++ turnamenbola/turnamen.java
@@ -1,1 +1,17 @@
+package turnamenbola;
+
+public class turnamen {
+  
+    public static void main(String[] args) {
+        
+        // format penulisan: <nama class> <nama object. bebas mau dinamakan apa> = new <nama class>
+        // bikin klub MU
+        klub klubEmyu = new klub("King Manchester United", "Budi", "Mikel", "Lamongan", 0);
+        klubEmyu.tampilkanIdentitasKlub();
+
+        klub klubUPN = new klub("UPN FC", "Johan", "James", "Surabaya", 5);
+        klubUPN.tampilkanIdentitasKlub();
+    }
+
+}
 
