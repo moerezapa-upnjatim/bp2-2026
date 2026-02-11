4e6e6f19c0032105cd2c9ed0d37207588c2b18ca
Muhammad Reza Pahlawan <muhammad_reza.si@upnjatim.ac.id>
2026-02-11 10:18:06 +0700

    Create klub.java

---
+++ turnamenbola/klub.java
@@ -0,0 +1,26 @@
+package turnamenbola;
+
+public class klub {
+    
+    String namaklub;
+    String namakapten;
+    String namapelatih;
+    String asalwilayah;
+    int jumlahtrofi;
+
+    // constructor
+    public klub(String namaklub, String namakapten, String namapelatih, String asalwilayah, int jumlahtrofi) {
+        // mengisi nilai variabel
+        this.namaklub = namaklub; // 'this' dipakai untuk memperjelas kita memanggil variabel class (di luar method)
+        this.namakapten = namakapten;
+        this.namapelatih = namapelatih;
+        this.asalwilayah = asalwilayah;
+        this.jumlahtrofi = jumlahtrofi;
+    }
+
+    public void tampilkanIdentitasKlub() {
+        System.out.printf("Nama klub: %s\n", namaklub);
+        System.out.printf("Nama kapten: %s\n", namakapten);
+        System.out.printf("Nama pelatih: %s\n", namapelatih);
+    }
+}
